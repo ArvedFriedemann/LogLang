377e695739e2116c9a7a5e3f3b441ae4d46ab6ff
Arved Friedemann <arved@uni-bremen.de>
2019-07-14 17:19:22 +0100

    [WIP] made some notes on how the streams might work. Did not got for.

--- log_comp/src/main.rs
+++ log_comp/src/main.rs
@@ -1,3 +1,27 @@
+trait Lazy_Term<'a, T>{
+    fn get_term(&self) -> &Term<'a, T>;
+}
+
+enum Term<'a, T> {
+    C(&'a Lazy_Term<'a, T>, &'a Lazy_Term<'a, T>),
+    A(T),
+}
+
+struct Unev_Term<'a, T> {
+    term: Option<>
+}
+
+impl<'a, T> Lazy_Term<'a, T> for Term<'a, T> {
+    fn get_term(&self) -> &Term<'a, T>{
+        return self;
+    }
+}
+
+//struct choice<'a, T> {
+//
+//}
+
+
 fn main() {
     println!("Hello, world!");
 }
