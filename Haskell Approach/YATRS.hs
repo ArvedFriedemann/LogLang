1d92853f168e0ba19909d731d33307cd909933b3
Arved Friedemann <arved@uni-bremen.de>
2019-07-19 17:27:40 +0100

    fixed parsing operators

--- Haskell Approach/YATRS.hs
+++ Haskell Approach/YATRS.hs
@@ -4,7 +4,8 @@ import Control.Monad
 import Text.Parsec hiding (spaces)
 
 data Term a = BOT | ATOM a | VAR a | APPL (Term a) (Term a) deriving (Eq, Show)
-data Equation a = EQTERM (Term a) | IMPL (Equation a) (Equation a) deriving (Eq, Show)
+
+impsymb = "->"
 
 spaces::Parsec String st String
 spaces = many $ oneOf "\t\n "
@@ -12,11 +13,17 @@ spaces = many $ oneOf "\t\n "
 spaces1::Parsec String st String
 spaces1 = many1 $ oneOf "\t\n "
 
+inlineSpaces1::Parsec String st String
+inlineSpaces1 = many1 $ oneOf "\t "
+
 termEnd::Parsec String st ()
 termEnd = lookAhead $ (void $ oneOf ")\n") <|> eof
 
 embrace::Parsec String st a -> Parsec String st b -> Parsec String st c -> Parsec String st b
 embrace b p b' = do {b; r<-p; b'; return r}
+
+parens::Parsec String st a -> Parsec String st a
+parens p = embrace (char '(') p (char ')')
 
 symbol::Parsec String st a -> Parsec String st a
 symbol p = embrace (many $ oneOf "\t ") p (many $ oneOf "\t ")
@@ -25,7 +32,7 @@ followedBy::Parsec String st b -> Parsec String st c -> Parsec String st b
 followedBy p b = do {r<-p; b; return r}
 
 parseIdfrName::Parsec String st String
-parseIdfrName = manyTill (noneOf "\t\n ()") (try $ string "->")
+parseIdfrName = manyTill anyChar $ lookAhead $ choice $ try <$> [void $ string impsymb, void $ string ".", void $ oneOf "\t\n ()", void $ eof]
 
 parseVar::Parsec String st (Term String)
 parseVar = do {
@@ -37,21 +44,19 @@ parseVar = do {
 parseAtom::Parsec String st (Term String)
 parseAtom = do {
   name <- parseIdfrName;
-  return $ ATOM $ name
+  case name of
+    [] -> fail ""
+    _ -> return $ ATOM $ name
 }
 
 parseIdfr::Parsec String st (Term String)
 parseIdfr = (try parseVar) <|> (try parseAtom) <|> (try (string "()" >> (return BOT)))
 
-parseTerms::Parsec String st [Term String]
-parseTerms = manyTill ((try $ embrace (char '(') parseTerm (char ')' >> spaces) )<|>(symbol parseIdfr)) (try termEnd);
+parseTerminal::Parsec String st (Term String)
+parseTerminal = parens parseTerm <|> parseIdfr
 
 parseTerm::Parsec String st (Term String)
-parseTerm = leftAssocTerm <$> (parseTerms `followedBy` spaces)
+parseTerm = parseTerminal `chainl1` ((try $ (inlineSpaces1 >> (lookAhead $ try $ parseTerminal)) ) >> (return APPL))
 
-leftAssocTerm::[Term a] -> Term a
-leftAssocTerm [] = BOT
-leftAssocTerm lst = foldl1 APPL lst
-
---parseEquation:: Parsec String st (Equation String)
---parseEquation = parseTerm `sepBy1` (string "->")
+parseImpl::Parsec String st (Term String)
+parseImpl = parseTerm `chainl1` ((symbol $ string impsymb) >> return (\x y -> APPL (APPL (VAR impsymb) x) y) )
